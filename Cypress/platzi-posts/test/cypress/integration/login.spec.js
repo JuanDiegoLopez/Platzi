acfecaa7f88fa818e57f2b8e5d5f5b9d4cac53f1
Juan Diego López <juandlopeztriana@gmail.com>
2018-11-04 12:34:50 -0500

    Cypress project added

---
+++ Cypress/platzi-posts/test/cypress/integration/login.spec.js
@@ -0,0 +1,40 @@
+'use strict'
+
+describe('Pruebas del login', () => {
+  before(() => {
+    cy.exec('npm run test:clean')
+  })
+
+  beforeEach (() => {
+    cy.fixture('user.json').as('userData')
+    cy.visit('/login')
+    cy.contains('h1', 'Bienvenido').should('be.visible')
+  })
+
+  it('Debe registrar un usuario', () => {
+      cy.get('@userData').then(userData => {
+        cy.createUser(userData)
+        cy.screenshot('create-user')
+      })
+  })
+
+  it('Debe fallar con un usuario erroneo', () => {
+    cy.loginUser('fail@test.com', 'test1234')
+    cy.get('.error-msg').should('be.visible')
+    cy.screenshot('login-failed', {
+      blackout: ['#email1']
+    })
+  })
+
+  it('Debe logear usuario', () => {
+      cy.get('@userData').then(userData => {
+        cy.loginUser(userData.email, userData.password)
+        cy.screenshot('login-user')
+        cy.contains('a', 'Dashboard').should('be.visible')
+      })
+  })
+
+  after(()=> {
+    cy.log('Test finalizados')
+  })
+})
